785967b47f3db0e733981e7d7a482db6efdcfd01
olwn <liechuan.ou@gmail.com>
2017-12-20 16:33:21 +0800

    callbacks

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import argparse
+import os
 from datetime import datetime
 import keras
-from keras.callbacks import LearningRateScheduler, TensorBoard
+from keras.callbacks import LearningRateScheduler, TensorBoard, CSVLogger, ModelCheckpoint
 from keras.optimizers import SGD
 from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+
 from resnet import ResnetBuilder
 
 
@@ -32,11 +35,33 @@ model = ResnetBuilder.build_resnet_cifar10(params.ly, use_bn=params.bn)
 sgd = SGD(momentum=0.9)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-lr_scheduler = LearningRateScheduler(lambda x: 0.05 if x < 60 else 0.03 if x < 80 else 0.005)
+
 dir_name = "e" + datetime.now().strftime("%m%d-%H-%M-%S") + \
            "layers" + str(params.ly) + \
            "_ing"
-
+csv_logger = CSVLogger(os.path.join(dir_name, 'log.csv'))
+lr_scheduler = LearningRateScheduler(lambda x: 0.05 if x < 60 else 0.03 if x < 80 else 0.005)
 board = TensorBoard(log_dir=dir_name, histogram_freq=0, write_graph=False, write_images=False)
-model.fit(x_train, y_train, batch_size=params.bs, epochs=params.ep)
-score = model.evaluate(x_test, y_test, batch_size=32)
+checker = ModelCheckpoint(filepath=os.path.join(dir_name, "weights.{epoch:02d}-{val_loss:.2f}.hdf5"), period=20)
+if not os.path.exists(dir_name): os.mkdir(dir_name)
+with open(os.path.join(dir_name, 'config'), 'w') as wf:
+    wf.write("-----" + str(params) + dir_name)
+callbacks = [lr_scheduler, csv_logger, board, checker]
+datagen_train = ImageDataGenerator(
+    width_shift_range=params.shift,
+    height_shift_range=params.shift,
+)
+datagen_test = ImageDataGenerator()
+model.fit_generator(datagen_train.flow(x_train, y_train, batch_size=params.bs),
+                    steps_per_epoch=x_train.shape[0] / params.bs,
+                    validation_data=datagen_test.flow(x_test, y_test, batch_size=params.bs),
+                    validation_steps=x_test.shape[0] / params.bs,
+                    epochs=params.ep,
+                    max_q_size=2500,
+                    workers=10,
+                    callbacks=callbacks)
+losses = model.evaluate(x_test, y_test, batch_size=params.batch_size)
+print(losses)
+with open(os.path.join(dir_name, 'config'), 'a') as wf:
+    wf.write(str(losses) + '\n')
+os.rename(dir_name, dir_name.replace('_ing', '_completed'))
